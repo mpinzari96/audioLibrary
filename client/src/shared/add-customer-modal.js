797199b4d59f9d64b9b4a23c08cdaa368e0ef635
Ion Pinzari <ipinzari@skyslope.com>
2017-08-22 10:15:39 -0700

    added validations

--- client/src/shared/add-customer-modal.js
+++ client/src/shared/add-customer-modal.js
@@ -10,51 +10,56 @@ var DatePicker = require("react-bootstrap-date-picker");
 import PropTypes from 'prop-types';
 
 @inject('appStore') @observer
- export default class AddCustomerModal extends Component{
+export default class AddCustomerModal extends Component {
   constructor(props) {
     super(props)
   }
 
   render() {
-      return (
-           <Modal
-          show={this.props.show}
-          onHide={this.props.onHide}>
-          {/*//   onEntered={ this.onModalEntered.bind(this) }>*/}
-          <Modal.Header closeButton >
-            <Modal.Title id="contained-modal-title-lg">Add New Customer</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form>
-              <FormGroup >
-                <ControlLabel>First Name</ControlLabel>
-                <FormControl
-                  type="text" placeholder="first Name" />
-              </FormGroup>
-              <FormGroup >
-                <ControlLabel>Last Name</ControlLabel>
-                <FormControl
-                  type="text" placeholder="last Name" />
-              </FormGroup>
-              <FormGroup >
-                <ControlLabel>Email</ControlLabel>
-                <FormControl
-                  type="email" placeholder="email" />
-              </FormGroup>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <button className='btn btn-default' onClick={this.props.onHide}>Cancel</button>
-            <button className='btn btn-primary'> Save </button>
-          </Modal.Footer>
-        </Modal>
-        )
+    return (
+      <Modal
+        show={this.props.show}
+        onHide={this.props.onHide}>
+        {/*//   onEntered={ this.onModalEntered.bind(this) }>*/}
+        <Modal.Header closeButton >
+          <Modal.Title id="contained-modal-title-lg">Add New Customer</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <FormGroup >
+              <ControlLabel>First Name</ControlLabel>
+              <FormControl
+                type="text" maxLength={20} placeholder="First Name" />
+            </FormGroup>
+            <FormGroup >
+              <ControlLabel>Last Name</ControlLabel>
+              <FormControl
+                type="text" maxLength={20} placeholder="Last Name" />
+            </FormGroup>
+            <FormGroup >
+              <ControlLabel>Phone Number</ControlLabel>
+              <FormControl
+                type="number" maxLength={10} placeholder="Phone Number" />
+            </FormGroup>
+            <FormGroup >
+              <ControlLabel>Email</ControlLabel>
+              <FormControl
+                type="email" maxLength={30} placeholder="Email" required={true} />
+            </FormGroup>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className='btn btn-default' onClick={this.props.onHide}>Cancel</Button>
+          <Button className='btn btn-primary' type='submit'> Save </Button>
+        </Modal.Footer>
+      </Modal>
+    )
 
-    }
   }
-AddCustomerModal.propTypes={
-show:PropTypes.bool.isRequired,
-onHide:PropTypes.func.isRequired
+}
+AddCustomerModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired
 
 };
 
